995dba1791dea0a1c39bb0cbda89e3188e756df3
arnoldlaraib <57247831+arnoldlaraib@users.noreply.github.com>
2024-07-29 23:15:16 +0530

    number_of_graded_assignments_for_each_student.sql

--- tests/SQL/number_of_graded_assignments_for_each_student.sql
+++ tests/SQL/number_of_graded_assignments_for_each_student.sql
@@ -1,1 +1,14 @@
 -- Write query to get number of graded assignments for each student:
+-- count_grade_A_assignments_by_teacher_with_max_grading.sql
+SELECT teacher_id, COUNT(*) AS grade_A_count
+FROM assignments
+WHERE grade = 'A'
+GROUP BY teacher_id
+ORDER BY grade_A_count DESC
+LIMIT 1;
+
+-- number_of_graded_assignments_for_each_student.sql
+SELECT student_id, COUNT(*) AS graded_assignments_count
+FROM assignments
+WHERE state = 'GRADED'
+GROUP BY student_id;
